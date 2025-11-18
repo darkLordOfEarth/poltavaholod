3a64fbf5957711c159acf1eee352bda5e58064fb
Leliksey <captainfroody@gmail.com>
2025-11-18 15:50:27 +0200

    3454454

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -11,40 +11,62 @@ $(document).ready(function() {
 
 
   $(function () {
-  const $video = $('.footer__video video');
-  const $playBtn = $('.footer__video .play__btn');
+  const $videos = $('video');
 
-  if ($video.length === 0) return; 
+  if ($videos.length === 0) return;
 
-  $playBtn.off('click');
-  $video.off('click');
+  $videos.each(function () {
+    const $video = $(this);
+    const $wrap = $video.closest('.videoBlock');
+    const $playBtn = $wrap.find('.play__btn');
 
-  $playBtn.on('click', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
+    $playBtn.off('click');
+    $video.off('click');
 
-    const vid = $video.get(0);
-    if (!vid) return;
+    $playBtn.on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
 
-    vid.play();
-    $playBtn.fadeOut(150);
-  });
+      const vid = $video.get(0);
+      if (!vid) return;
 
-  $video.on('click', function (e) {
-    const vid = this;
-
-    if (vid.paused) {
       vid.play();
       $playBtn.fadeOut(150);
-    } else {
-      vid.pause();
+    });
+
+    $video.on('click', function () {
+      const vid = this;
+
+      if (vid.paused) {
+        vid.play();
+        $playBtn.fadeOut(150);
+      } else {
+        vid.pause();
+        $playBtn.fadeIn(150);
+      }
+    });
+
+    $video.on('ended', function () {
       $playBtn.fadeIn(150);
-    }
-  });
-  $video.on('ended', function () {
-    $playBtn.fadeIn(150);
+    });
   });
 });
+
+
+
+$('.virtualTour__toggle-btn').on('click', function () {
+  $('.virtualTour__toggle-btn').removeClass('active');
+  $(this).addClass('active');
+  let video = $(this).attr("data-video");
+  let link = $(this).attr("data-link");
+  let elem = $(".virtualTour .video-element");
+  let btn = $(".virtualTour__link");
+  elem.attr("src", video);
+  btn.attr("href", link);
+  let $play = $(this).parents(".virtualTour").find(".play__btn");
+  $play.fadeIn(150);
+});
+
 
 
 });
