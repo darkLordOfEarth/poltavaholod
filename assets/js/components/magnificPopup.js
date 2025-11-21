e0d5a3e90f516ead96d30805f284cc26e072a180
Leliksey <captainfroody@gmail.com>
2025-11-21 14:42:33 +0200

    3453rf3rt34t23

--- assets/js/components/magnificPopup.js
+++ assets/js/components/magnificPopup.js
@@ -1,15 +1,23 @@
 $(function () {
-    $('.template1__image').magnificPopup({
-        delegate: 'a.gallery-item', // внутри контейнера
-        type: 'image',
-        gallery: {
-            enabled: true,        // включаем галерею
-            navigateByImgClick: true,
-            preload: [0, 1]       // загрузка текущей и следующей картинки
+    // $('.template1__image').magnificPopup({
+    //     delegate: 'a.gallery-item', // внутри контейнера
+    //     type: 'image',
+    //     gallery: {
+    //         enabled: true,        // включаем галерею
+    //         navigateByImgClick: true,
+    //         preload: [0, 1]       // загрузка текущей и следующей картинки
+    //     },
+    //     removalDelay: 200,
+    //     mainClass: 'mfp-fade'
+    // });
+
+
+    Fancybox.bind("[data-fancybox='gallery']", {
+        Thumbs: {
+            autoStart: true, // миниатюры сразу открываются снизу
         },
-        removalDelay: 200,
-        mainClass: 'mfp-fade'
     });
+
 });
 
 
